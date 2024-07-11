8a0c410064f4d7f449d481c3844342f4ebcd6f02
Marcocadas <86499030+Marcocadas@users.noreply.github.com>
2024-07-11 14:10:02 -0400

    Variables con VAR
    Aqui vimos unos ejemplos del uso de var, el cual sirve para definir variables en JS, esta forma de definir variables ya no se utiliza actualmente.

---
+++ js/01.js
@@ -0,0 +1,43 @@
+
+/* 
+Así se comenta mas de una linea de código
+*/
+/* Recomendacion del profe siempre guardar los scripts
+porque en una entrevista es más sencillo revisar el código que
+empezar denuevo el curso
+*/
+
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; // Iniciamos la variable pero sin valor
+
+producto = true; // reasignando el valor de la variable
+
+disponible = true; 
+
+var producto1 = 'Computadora',
+    disponible1= true,
+    categoria = 'Computadoras';
+
+
+// var 1disponible; no es posible 
+// var -disponible; tampoco es posible
+var _disponible;
+// Estilos para variables:
+var nombre_producto = 'Monitor HD'; // Underscore
+var nombreProducto = 'Monitor HD'; // Camelcase
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD'; // Normal
+
+console.log(producto);
+
+// Esta forma de definir variables ya no se utiliza
+// JS No es estricto con los punto y coma
+// En JS no es necesario declarar el tipo de dato
+// JS es un lenguaje de tipo dinamico, mientras se ejecuta se compila
+// Las variables pueden tener, letras, números y caracteres, pero no pueden iniciar con carecteres o números.
+
+
+
