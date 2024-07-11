bdd479b9a2d3ac48582a1bcbdae71d1c3b490c85
Marcocadas <86499030+Marcocadas@users.noreply.github.com>
2024-07-11 14:19:46 -0400

    let y const
    let no se diferencia de var, pero const si, ya que no puedes iniciar una variable sin un valor y no puedes reasignarle un valor a una misma variable.

---
+++ js/02.js
@@ -0,0 +1,44 @@
+
+/* 
+Así se comenta mas de una linea de código
+*/
+/* Recomendacion del profe siempre guardar los scripts
+porque en una entrevista es más sencillo revisar el código que
+empezar denuevo el curso
+*/
+
+// Variables con let
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+let disponible; // Iniciamos la variable pero sin valor
+
+producto = true; // reasignando el valor de la variable
+
+disponible = true; 
+
+let producto1 = 'Computadora',
+    disponible1= true,
+    categoria = 'Computadoras';
+
+
+// let 1disponible; no es posible 
+// let -disponible; tampoco es posible
+let _disponible;
+// Estilos para variables:
+let nombre_producto = 'Monitor HD'; // Underscore
+let nombreProducto = 'Monitor HD'; // Camelcase
+let NombreProducto = 'Monitor HD'; // Pascal Case
+let nombreproducto = 'Monitor HD'; // Normal
+
+console.log(producto);
+
+// Esta forma de definir variables ya no se utiliza
+// JS No es estricto con los punto y coma
+// En JS no es necesario declarar el tipo de dato
+// JS es un lenguaje de tipo dinamico, mientras se ejecuta se compila
+// Las variables pueden tener, letras, números y caracteres, pero no pueden iniciar con carecteres o números.
+// Al definir variables deben de escribirse igual al usarlas
+
+// Entre var y let no hay diferencias, siguen las mismas reglas.
+
---
+++ js/03.js
@@ -0,0 +1,38 @@
+
+/* 
+Así se comenta mas de una linea de código
+*/
+/* Recomendacion del profe siempre guardar los scripts
+porque en una entrevista es más sencillo revisar el código que
+empezar denuevo el curso
+*/
+
+// Variables con const
+
+const producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+const disponible; //NO SE PUEDE INICIAR UNA VARIABLE SIN VALOR
+
+producto = true; // NO PUEDES REASIGNAR EL VALOR
+
+disponible = true; 
+
+const producto1 = 'Computadora',
+    disponible1= true,
+    categoria = 'Computadoras';
+
+
+// const 1disponible; no es posible 
+// const -disponible; tampoco es posible
+const _disponible = true;
+// Estilos para variables:
+const nombre_producto = 'Monitor HD'; // Underscore
+const nombreProducto = 'Monitor HD'; // Camelcase
+const NombreProducto = 'Monitor HD'; // Pascal Case
+const nombreproducto = 'Monitor HD'; // Normal
+
+console.log(producto);
+
+
+
+
